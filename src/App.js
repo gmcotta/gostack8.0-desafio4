bc87d0cdc074628ea8955cb0a520f095e44c7479
Gustavo Cotta <gustavocotta@Gustavos-iMac.local>
2019-09-18 22:44:45 -0300

    Importing images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 import "./App.css";
+import profile from "./assets/profile_f_2.jpg";
 
 function App() {
-  return <h1>Hello Rocketseat</h1>;
+  return (
+    <>
+      <h1>Hello Rocketseat</h1>
+      <img src={profile} />
+    </>
+  );
 }
 
 export default App;
